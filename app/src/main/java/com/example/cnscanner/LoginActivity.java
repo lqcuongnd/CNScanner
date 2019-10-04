87835f15710dd0ffd8d624c9a534b83c4981651c
Huynh Nhu <huynhnhutran114@gmail.com>
2019-10-04 20:29:26 +0700

    Login

--- app/src/main/java/com/example/cnscanner/LoginActivity.java
+++ app/src/main/java/com/example/cnscanner/LoginActivity.java
@@ -2,13 +2,68 @@ package com.example.cnscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView txt1;
+    EditText editname, editpass;
+    Button btnlogin, btncacel;
+    ImageView imageView;
+    int counter = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        addControl();
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editname.getText().toString().equals("admin") &&
+                        editpass.getText().toString().equals("123")) {
+                    Toast.makeText(LoginActivity.this, "Thành công", Toast.LENGTH_SHORT).show();
+                   // Intent intent = new Intent(getApplicationContext(), Manager.class);
+                   // startActivity(intent);
+                }else{
+                    Toast.makeText(LoginActivity.this, "Thất bại", Toast.LENGTH_SHORT).show();
+
+                    txt1.setVisibility(View.VISIBLE);
+                    txt1.setBackgroundColor(Color.RED);
+                    counter--;
+                    txt1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        btnlogin.setEnabled(false);
+                    }
+                }
+            }
+        });
+        btncacel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
+
+    private void addControl() {
+        txt1 = (TextView) findViewById(R.id.textview);
+        editname = (EditText) findViewById(R.id.editname);
+        editpass = (EditText) findViewById(R.id.editpass);
+        btnlogin = (Button) findViewById(R.id.btnlogin);
+        btncacel = (Button) findViewById(R.id.btncancel);
+        imageView = (ImageView) findViewById(R.id.imageView);
+    }
+
+
+
 }
--- app/src/main/java/com/example/cnscanner/MainActivity.java
+++ app/src/main/java/com/example/cnscanner/MainActivity.java
@@ -34,40 +34,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        intent = new Intent(this, WelcomeActivity.class);
-        startActivityForResult(intent, WELCOME);
+      //  intent = new Intent(this, WelcomeActivity.class);
+      //  startActivityForResult(intent, WELCOME);
 
-        if (!isLogged()) {
-            intent = new Intent(this, LoginActivity.class);
-            startActivityForResult(intent, LOGIN);
-        }
-    }
+      //  if (!isLogged()) {
+          //  intent = new Intent(this, LoginActivity.class);
+          //  startActivityForResult(intent, LOGIN);
+        //}
+    //}
 
     //Hàm kiểm tra đã đăng nhập hay chưa
-    private boolean isLogged() {
-        if (true) {
+  //  private boolean isLogged() {
+      //  if (true) {
             //Đã đăng nhập
-            return true;
-        }
-        return false;
-    }
+            //return true;
+       // }
+      //  return false;
+   // }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+  //  @Override
+  //  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+   //     super.onActivityResult(requestCode, resultCode, data);
 
-        switch (requestCode) {
-            case WELCOME:
-                if (resultCode == RESULT_WELCOME) {
+      //  switch (requestCode) {
+       //     case WELCOME:
+              //  if (resultCode == RESULT_WELCOME) {
 
-                }
-                break;
-            case LOGIN:
-                if (resultCode == RESULT_LOGIN) {
+               // }
+              //  break;
+            //case LOGIN:
+             //   if (resultCode == RESULT_LOGIN) {
 
-                }
-                break;
+             //   }
+                //break;
 
-        }
+       // }
     }
 }
